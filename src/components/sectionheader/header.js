d10a0e0e93ab59140cb80e2f649c7b13a3790c37
Tornike-Kiknadze <tkiknaze@yahoo.com>
2019-03-24 20:16:38 +0400

    translations

--- src/components/sectionheader/header.js
+++ src/components/sectionheader/header.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
+import { FormattedMessage } from "react-intl";
+import "./header.css";
 
-import './header.css';
+const Header = props => {
+  let { header, title } = props;
 
-const Header = (props)=> {
-   let {header, title} =props;
-
- 
-
- 
-        return (
-            <div className='header-wrapper'> 
-                <p className={'products-header ' + (props.styleHeader)}>{header}</p>
-                <p className={'products-title ' + (props.styleTitle)} >{title}</p>
-                <div className='products-image'><img alt='' src={require('./assets/img/glasses.png')}></img></div>
-
-                </div>
-          
-        );
-    }
- 
+  return (
+    <div className="header-wrapper">
+      <p className={"products-header " + props.styleHeader}>{header}</p>
+      <p className={"products-title " + props.styleTitle}>
+        {" "}
+        <FormattedMessage id={`${title}`} />
+      </p>
+      <div className="products-image">
+        <img alt="" src={require("./assets/img/glasses.png")} />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
